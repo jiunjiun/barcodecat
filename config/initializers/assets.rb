97adac71dbfb66373305fa695f3f618f6bba5c4c
jiunjiun <quietmes@gmail.com>
2017-01-30 13:23:41 +0800

    Add - Add home page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( admin.css admin.js )
 Rails.application.config.assets.precompile += %w( paralax.css paralax.js )
+Rails.application.config.assets.precompile += %w( home.css home.js )
