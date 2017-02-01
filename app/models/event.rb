433156c3ad08006e7a72a06910eb5392baa2aa4f
jiunjiun <quietmes@gmail.com>
2017-02-01 14:59:57 +0800

    Refine - Refine event keywords space

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,11 @@ class Event < ApplicationRecord
   validates_presence_of :title, :link_name, :keywords
 
   mount_uploader :image, ProductBannerUploader
+
+  before_save :fiter_keywords
+
+  private
+  def fiter_keywords
+    self.keywords = self.keywords.delete(' ')
+  end
 end
