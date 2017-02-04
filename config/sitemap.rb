f0fd37cb1a4bd2adda228f65a808ad9718c771af
jiunjiun <quietmes@gmail.com>
2017-02-05 03:02:53 +0800

    Reifne - Refine sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -3,6 +3,6 @@ SitemapGenerator::Sitemap.default_host = Settings.host
 
 SitemapGenerator::Sitemap.create do
   Event.where(enable: true).each do |evnet|
-    add event_path(evnet.link_name), priority: 1.0, changefreq: 'always', lastmod => evnet.updated_at
+    add event_path(evnet.link_name), priority: 1.0, changefreq: 'always', lastmod: evnet.updated_at
   end
 end
