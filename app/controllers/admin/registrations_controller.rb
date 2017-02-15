a41aceeea9422bb6e5a879bad607c5f0ab8803d5
jiunjiun <quietmes@gmail.com>
2017-02-15 19:58:07 +0800

    Add - Add admin password change

---
+++ app/controllers/admin/registrations_controller.rb
@@ -0,0 +1,3 @@
+class Admin::RegistrationsController < Devise::RegistrationsController
+  layout 'sign_in'
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  devise_for :admins, controllers: {sessions: 'admin/sessions'}
-  namespace :admin, path: '/', constraints: {subdomain: "admin"} do
+  devise_for :admins, controllers: { sessions: 'admin/sessions' }
+  namespace :admin, path: '/', constraints: { subdomain: 'admin' } do
+    devise_scope :admin do
+      get 'passwords/edit', to: 'registrations#edit',   as: 'edit_passwords'
+      put 'passwords',      to: 'registrations#update', as: 'passwords'
+    end
+
     root 'dashboard#index'
 
     resources :events do
